08389a3809129276917eef075f175e7f238cc137
artt1c <roma.myalo000@gmail.com>
2026-08-01 19:30:59 +0300

    Init

---
+++ BlacJackMobile/BlacJackMobile/BlacJackMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlacJackMobileApp.swift
+//  BlacJackMobile
+//
+//  Created by Admin on 01.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct BlacJackMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
